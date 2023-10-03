61cc2813ec45792752a5a9f696231d6ee20f6c25
hadez16 <nico.ballat@t-online.de>
2023-10-03 10:59:06 +0200

    added support for 2 byte addresses

--- AutoScanCompare/Resources/WinFormStrings.Designer.cs
+++ AutoScanCompare/Resources/WinFormStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace CodingCompare.Resources {
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class WinFormStrings {
@@ -187,7 +187,7 @@ namespace CodingCompare.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die ^Address [0-9A-Z][0-9A-Z](?=\:) ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die ^Address [0-9A-Z]{2,4}(?=\:) ähnelt.
         /// </summary>
         internal static string str_autoscanValidation_Regex {
             get {
